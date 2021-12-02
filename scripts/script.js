fd36f8da9f9fc5251ea6baa13366cecdf32f65b5
DonnaDivana <94406180+DonnaDivana@users.noreply.github.com>
2021-12-02 09:11:54 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,21 @@
 // JavaScript Document
+
+var mobielNav = document.querySelector('nav'); 
+var hamburgerIcoon = document.querySelector('header div');
+
+var dragonTurn = document.querySelector('section:nth-of-type(2) img')
+
+
+function navUitklappen() {
+    mobielNav.classList.toggle("uitgeklapt");
+}
+
+function draaien() {
+    dragonTurn.classList.toggle("draaien"); 
+}
+
+
+
+hamburgerIcoon.addEventListener('click', navUitklappen);
+dragonTurn.addEventListener("click", draaien);
+
